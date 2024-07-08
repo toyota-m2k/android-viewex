41f32d6032b5053266ed483a2cbca69a102c0ef4
toyota-m2k <toyota.mitsuki@gmail.com>
2024-07-08 16:22:52 +0900

    chore: version catalog

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -32,19 +32,18 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    val lifecycleKtxVersion = "2.8.3"
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleKtxVersion")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleKtxVersion")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleKtxVersion")
-    implementation("com.github.toyota-m2k:android-utilities:2.0.2")
-    implementation("com.github.toyota-m2k:android-binding:1.1.3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    implementation(libs.coreKtx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.constraintLayout)
+    implementation(libs.lifecycleExtensions)
+    implementation(libs.lifecycleRuntimeKtx)
+    implementation(libs.lifecycleLiveDataKtx)
+    implementation(libs.lifecycleViewModelKtx)
+    implementation(libs.android.utilities)
+    implementation(libs.android.binding)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidxJunit)
+    androidTestImplementation(libs.espressoCore)
     implementation(project(":library"))
 }
