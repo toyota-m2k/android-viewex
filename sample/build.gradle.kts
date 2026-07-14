c4c7acb3f510049c8d39abceb554bfc68b356e2b
toyota-m2k <toyota.mitsuki@gmail.com>
2026-07-14 19:59:10 +0900

    chore: sdk 37 + jdk 21

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 configure<ApplicationExtension> {
     namespace = "io.github.toyota32k.viewex"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "io.github.toyota32k.viewex"
         minSdk = 23
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -24,9 +24,13 @@ configure<ApplicationExtension> {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
+}
+
+kotlin {
+    jvmToolchain(21)
 }
 
 dependencies {
